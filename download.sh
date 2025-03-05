eba93b64562bc7c5a2f3a8b1b8cc5a7eb41f8d18
Nathan Huey <nathanhuey44@gmail.com>
2025-03-05 05:08:38 +0000

    Add download script for datasets
    To ensure the download script works correctly, ensure that you are in
    the root of the repository when running the script.

---
+++ download.sh
@@ -0,0 +1,8 @@
+wget -P ./graphchi-c/data/ego-Facebook/ https://snap.stanford.edu/data/facebook_combined.txt.gz
+gunzip graphchi-c/data/ego-Facebook/facebook_combined.txt.gz
+
+wget -P ./graphchi-c/data/soc-Epinions1/ https://snap.stanford.edu/data/soc-Epinions1.txt.gz
+gunzip graphchi-c/data/soc-Epinions1/soc-Epinions1.txt.gz
+
+wget -P ./graphchi-c/data/web-Google/ https://snap.stanford.edu/data/web-Google.txt.gz
+gunzip graphchi-c/data/web-Google/web-Google.txt.gz
